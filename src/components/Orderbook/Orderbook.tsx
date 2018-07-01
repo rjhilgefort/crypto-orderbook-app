efaef89e6c2ef127d3db6e2bcc3d2fc73470f0b8
Rob Hilgefort <rjhilgefort@gmail.com>
2018-07-01 00:56:00 -0600

    both graphs

--- src/components/Orderbook/Orderbook.tsx
+++ src/components/Orderbook/Orderbook.tsx
@@ -7,31 +7,54 @@ import { IPromiseState } from 'src/types/react-refetch';
 import _ from 'src/utils';
 import { GridStyled } from './style';
 
+// TODO: Move to own thing
+const Graph = ({
+  title,
+  data,
+}: {
+  title: string,
+  data: object[],
+}) => (
+  <React.Fragment>
+    <h1>{title}</h1>
+    <AreaChart
+      width={1000} height={600}
+      data={data}
+    >
+      <CartesianGrid strokeDasharray="3 3"/>
+      <XAxis
+        label="Rate"
+        tickFormatter={_.take(6)}
+        domain={['dataMin - 0.005', 'dataMax + 0.005']}
+        dataKey="name"
+      />
+      <YAxis
+        label="Quantity"
+        tickFormatter={_.toFixed(0)}
+        domain={['dataMin - 5', 'dataMax + 20']}
+      />
+      <Tooltip/>
+      <Area type='monotone' dataKey='value' stroke='#8884d8' fill='#8884d8' />
+    </AreaChart>
+  </React.Fragment>
+);
+
 export interface IProps {
   orderbookFetch: IPromiseState<CombinedOrderbook>;
 }
 const Orderbook: React.SFC<IProps> = ({ orderbookFetch }) => (
   <GridStyled>
     <Grid.Row>
-      <AreaChart
-        width={1000} height={600}
+      <Graph
+        title="Bids"
         data={orderbookFetch.value.bids}
-      >
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis
-          label="Rate"
-          tickFormatter={_.take(6)}
-          domain={['dataMin - 0.005', 'dataMax + 0.005']}
-          dataKey="name"
-        />
-        <YAxis
-          label="Quantity"
-          tickFormatter={_.toFixed(0)}
-          domain={['dataMin - 5', 'dataMax + 20']}
-        />
-        <Tooltip/>
-        <Area type='monotone' dataKey='value' stroke='#8884d8' fill='#8884d8' />
-      </AreaChart>
+      />
+    </Grid.Row>
+    <Grid.Row>
+      <Graph
+        title="Asks"
+        data={orderbookFetch.value.asks}
+      />
     </Grid.Row>
   </GridStyled>
 );
